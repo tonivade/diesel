607e8a3d1af97c8b5c809884ff9242630911b3eb
Antonio Muñoz <antoniogmc@gmail.com>
2025-01-29 21:59:25 +0100

    code cleanup

--- src/main/java/com/github/tonivade/diesel/Reference.java
+++ src/main/java/com/github/tonivade/diesel/Reference.java
@@ -12,9 +12,6 @@ public sealed interface Reference<V, T> extends Program.Dsl<Reference.Service<V>
   interface Service<V> {
     void set(V value);
     V get();
-    default void update(UnaryOperator<V> update) {
-      set(update.apply(get()));
-    }
   }
 
   record SetValue<V>(V value) implements Reference<V, Void> {}
