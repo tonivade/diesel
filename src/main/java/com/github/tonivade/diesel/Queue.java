1812bdb509032ecdf97f53cebfde6f2802563427
Antonio Muñoz <antoniogmc@gmail.com>
2025-02-16 23:21:36 +0100

    more javadoc

--- src/main/java/com/github/tonivade/diesel/Queue.java
+++ src/main/java/com/github/tonivade/diesel/Queue.java
@@ -6,35 +6,96 @@ package com.github.tonivade.diesel;
 
 import static com.github.tonivade.diesel.Result.success;
 
+/**
+ * Represents a program that can be executed on a {@link Service} that provides
+ * queue operations.
+ * This interface extends {@link Program.Dsl} to provide a domain-specific
+ * language (DSL) for working with queues.
+ *
+ * @param <V> the type of items in the queue
+ * @param <T> the type of result returned by the queue operation
+ */
 public sealed interface Queue<V, T> extends Program.Dsl<Queue.Service<V>, Void, T> {
 
+  /**
+   * Defines the contract for a queue service.
+   *
+   * @param <V> the type of items in the queue
+   */
   interface Service<V> {
+    /**
+     * Adds an item to the queue.
+     *
+     * @param item the item to add
+     */
     void offer(V item);
+
+    /**
+     * Removes and returns the next item from the queue.
+     *
+     * @return the next item from the queue
+     */
     V take();
   }
 
+  /**
+   * Represents an operation to add an item to the queue.
+   *
+   * @param <V> the type of items in the queue
+   */
   record Offer<V>(V item) implements Queue<V, Void> {}
+
+  /**
+   * Represents an operation to remove and return the next item from the queue.
+   *
+   * @param <V> the type of items in the queue
+   */
   record Take<V>() implements Queue<V, V> {}
 
+  /**
+   * Creates a {@link Program} instance that adds an item to the queue.
+   *
+   * @param <V>  the type of items in the queue
+   * @param <S>  the type of service
+   * @param <E>  the type of error
+   * @param item the item to add
+   * @return a {@link Program} instance that adds an item to the queue
+   */
   @SuppressWarnings("unchecked")
   static <V, S extends Service<V>, E> Program<S, E, Void> offer(V item) {
     return (Program<S, E, Void>) new Offer<>(item);
   }
 
+  /**
+   * Creates a {@link Program} instance that removes and returns the next item
+   * from the queue.
+   *
+   * @param <V> the type of items in the queue
+   * @param <S> the type of service
+   * @param <E> the type of error
+   * @return a {@link Program} instance that removes and returns the next item
+   *         from the queue
+   */
   @SuppressWarnings("unchecked")
   static <V, S extends Service<V>, E> Program<S, E, V> take() {
     return (Program<S, E, V>) new Take<>();
   }
 
+  /**
+   * Evaluates the queue program on a given service.
+   *
+   * @param service the service to evaluate the program on
+   * @return the result of the evaluation
+   */
   @Override
   @SuppressWarnings({ "rawtypes", "unchecked" })
-  default Result<Void, T> dslEval(Service<V> state) {
+  default Result<Void, T> dslEval(Service<V> service) {
     return success((T) switch (this) {
       case Offer offer -> {
-        state.offer((V) offer.item());
+        service.offer((V) offer.item());
         yield null;
       }
-      case Take take -> state.take();
+      case Take take -> service.take();
     });
   }
 }
--- src/main/java/com/github/tonivade/diesel/Result.java
+++ src/main/java/com/github/tonivade/diesel/Result.java
@@ -9,23 +9,78 @@ import java.util.function.BiFunction;
 import java.util.function.Function;
 import org.jspecify.annotations.Nullable;
 
+/**
+ * The Result type is used to represent a value that may or may not be present.
+ * This type is useful for handling errors and exceptions in a more functional programming style.
+ *
+ * @param <F> The type of the failure value.
+ * @param <S> The type of the success value.
+ */
 public sealed interface Result<F, S> {
 
+  /**
+   * Creates a new failure result.
+   *
+   * @param error The error value.
+   * @param <F> The type of the failure value.
+   * @param <S> The type of the success value.
+   * @return A new failure result.
+   */
   static <F, S> Result<F, S> failure(F error) {
     return new Failure<>(error);
   }
 
+  /**
+   * Creates a new success result.
+   *
+   * @param value The success value, which may be null.
+   * @param <F> The type of the failure value.
+   * @param <S> The type of the success value.
+   * @return A new success result.
+   */
   static <F, S> Result<F, S> success(@Nullable S value) {
     return new Success<>(value);
   }
 
+  /**
+   * Creates a new result by combining two results using the provided mapper function.
+   *
+   * @param r1 The first result.
+   * @param r2 The second result.
+   * @param mapper The function used to combine the success values of the two results.
+   * @param <F> The type of the failure value.
+   * @param <S> The type of the success value of the first result.
+   * @param <T> The type of the success value of the second result.
+   * @param <R> The type of the success value of the combined result.
+   * @return A new result that combines the two input results.
+   */
   static <F, S, T, R> Result<F, R> zip(Result<F, S> r1, Result<F, T> r2, BiFunction<S, T, R> mapper) {
     return r1.flatMap(a -> r2.map(b -> mapper.apply(a, b)));
   }
 
+  /**
+   * A record representing a failure result.
+   *
+   * @param <F> The type of the failure value.
+   * @param <S> The type of the success value.
+   */
   record Failure<F, S>(F error) implements Result<F, S> {}
+
+  /**
+   * A record representing a success result.
+   *
+   * @param <F> The type of the failure value.
+   * @param <S> The type of the success value.
+   */
   record Success<F, S>(@Nullable S value) implements Result<F, S> {}
 
+  /**
+   * Maps the success value of this result using the provided mapper function.
+   *
+   * @param mapper The function used to map the success value.
+   * @param <R> The type of the mapped success value.
+   * @return A new result with the mapped success value.
+   */
   @SuppressWarnings("unchecked")
   default <R> Result<F, R> map(Function<S, R> mapper) {
     return switch (this) {
@@ -34,6 +89,13 @@ public sealed interface Result<F, S> {
     };
   }
 
+  /**
+   * Maps the failure value of this result using the provided mapper function.
+   *
+   * @param mapper The function used to map the failure value.
+   * @param <R> The type of the mapped failure value.
+   * @return A new result with the mapped failure value.
+   */
   @SuppressWarnings("unchecked")
   default <R> Result<R, S> mapError(Function<F, R> mapper) {
     return switch (this) {
@@ -42,6 +104,13 @@ public sealed interface Result<F, S> {
     };
   }
 
+  /**
+   * Maps the success value of this result using the provided mapper function and returns a new result.
+   *
+   * @param mapper The function used to map the success value.
+   * @param <R> The type of the mapped success value.
+   * @return A new result with the mapped success value.
+   */
   @SuppressWarnings("unchecked")
   default <R> Result<F, R> flatMap(Function<S, Result<F, R>> mapper) {
     return switch (this) {
@@ -50,6 +119,14 @@ public sealed interface Result<F, S> {
     };
   }
 
+  /**
+   * Folds the result into a single value using the provided functions.
+   *
+   * @param onFailure The function used to handle the failure value.
+   * @param onSuccess The function used to handle the success value.
+   * @param <R> The type of the folded value.
+   * @return The folded value.
+   */
   default <R> R fold(Function<F, R> onFailure, Function<S, R> onSuccess) {
     return switch (this) {
       case Failure<F, S>(F error) -> onFailure.apply(error);
@@ -57,10 +134,24 @@ public sealed interface Result<F, S> {
     };
   }
 
+  /**
+   * Returns the success value of this result, throwing a NoSuchElementException if this is a failure.
+   *
+   * @return The success value of this result.
+   * @throws NoSuchElementException If this is a failure.
+   */
   default S getOrElseThrow() {
     return getOrElseThrow(__ -> new NoSuchElementException());
   }
 
+  /**
+   * Returns the success value of this result, throwing an exception if this is a failure.
+   *
+   * @param mapper The function used to map the failure value to an exception.
+   * @param <X> The type of the exception.
+   * @return The success value of this result.
+   * @throws X If this is a failure.
+   */
   default <X extends Throwable> S getOrElseThrow(Function<F, X> mapper) throws X {
     return switch (this) {
       case Success<F, S>(S value) -> value;
