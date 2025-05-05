f584c6fbf06975a7038ec9519f2fb35282723d3c
Antonio Muñoz <antonio.munoz@clarity.ai>
2025-05-05 12:45:29 +0200

    add javadoc

--- src/main/java/com/github/tonivade/diesel/Trampoline.java
+++ src/main/java/com/github/tonivade/diesel/Trampoline.java
@@ -6,43 +6,102 @@ package com.github.tonivade.diesel;
 
 import static java.util.function.Function.identity;
 
-import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-public sealed interface Trampoline<T> {
+/**
+ * A Trampoline is a data structure used to implement tail recursion in Java.
+ * It represents a computation that may yield a result or continue with another
+ * computation. This allows for efficient and stack-safe recursive computations.
+ *
+ * @param <T> the type of the result of the computation
+ */
+sealed interface Trampoline<T> {
 
+  /**
+   * A Done Trampoline represents a successful computation with a given result.
+   *
+   * @param <T> the type of the result
+   */
   record Done<T>(T value) implements Trampoline<T> {}
+
+  /**
+   * A More Trampoline represents a computation that continues with another
+   * computation based on its result.
+   *
+   * @param <T> the type of the result of this computation
+   * @param <R> the type of the result of the next computation
+   */
   record More<T>(Supplier<Trampoline<T>> next) implements Trampoline<T> {}
 
+  /**
+   * Creates a Done Trampoline with a given value.
+   *
+   * @param <T> the type of the value
+   * @param value the value of the computation
+   * @return a Done Trampoline with the given value
+   */
   static <T> Trampoline<T> done(T value) {
     return new Done<>(value);
   }
 
+  /**
+   * Creates a Trampoline that continues with a supplier of another Trampoline.
+   *
+   * @param <T> the type of the result of the computation
+   * @param next a supplier of another Trampoline
+   * @return a Trampoline that continues with the supplier of another Trampoline
+   */
   static <T> Trampoline<T> more(Supplier<Trampoline<T>> next) {
     return new More<>(next);
   }
 
+  /**
+   * Maps the result of this computation using a given function.
+   *
+   * @param <R> the type of the result of the function
+   * @param mapper the function to apply to the result
+   * @return a new Trampoline with the mapped result
+   */
   default <R> Trampoline<R> map(Function<T, R> mapper) {
     return flatMap(mapper.andThen(Trampoline::done));
   }
 
-  default <R> Trampoline<R> flatMap(Function<T, Trampoline<R>> mapper) {
-    return fold(next -> more(() -> next.flatMap(mapper)), mapper);
-  }
-
+  /**
+   * Chains two Trampolines together, discarding the result of the first one.
+   *
+   * @param <R> the type of the result of the next Trampoline
+   * @param next the next Trampoline
+   * @return a new Trampoline that chains the two computations together
+   */
   default <R> Trampoline<R> andThen(Trampoline<R> next) {
     return flatMap(__ -> next);
   }
 
-  default <R> R fold(Function<Trampoline<T>, R> moreMapper, Function<T, R> doneMapper) {
+  /**
+   * Flat maps the result of this computation using a given function.
+   *
+   * @param <R> the type of the result of the function
+   * @param next the function to apply to the result
+   * @return a new Trampoline with the flat mapped result
+   */
+  default <R> Trampoline<R> flatMap(Function<T, Trampoline<R>> mapper) {
+    return fold(next -> more(() -> next.flatMap(mapper)), mapper);
+  }
+
+  private <R> R fold(Function<Trampoline<T>, R> moreMapper, Function<T, R> doneMapper) {
     return switch (this) {
       case Done<T>(var value) -> doneMapper.apply(value);
       case More<T>(var next) -> moreMapper.apply(next.get());
     };
   }
 
+  /**
+   * Runs the computation and returns the final result.
+   *
+   * @return the final result of the computation
+   */
   default T run() {
     return iterate().fold(__ -> {
       throw new IllegalStateException();
@@ -52,9 +111,5 @@ public sealed interface Trampoline<T> {
   private Trampoline<T> iterate() {
     return Stream.iterate(this, t -> t.fold(identity(), __ -> t))
         .dropWhile(t -> t instanceof More).findFirst().orElseThrow();
-  }
-
-  static <A, B, R> Trampoline<R> map2(Trampoline<A> ta, Trampoline<B> tb, BiFunction<A, B, R> mapper) {
-    return ta.flatMap(a -> tb.map(b -> mapper.apply(a, b)));
   }
 }
