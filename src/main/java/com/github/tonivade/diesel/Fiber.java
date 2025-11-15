fe10f55ce80d60b48ff3af41adf6783cff20fb63
Antonio Muñoz <antoniogmc@gmail.com>
2025-11-15 23:24:55 +0100

    future not exposed outside

--- src/main/java/com/github/tonivade/diesel/Fiber.java
+++ src/main/java/com/github/tonivade/diesel/Fiber.java
@@ -27,29 +27,91 @@ import java.util.function.Function;
  * @param <E> the type of the error
  * @param <T> the type of the result
  */
-public record Fiber<E, T>(CompletableFuture<Result<E, T>> future) {
+public final class Fiber<E, T> {
+  
+  private final CompletableFuture<Result<E, T>> future;
+  
+  Fiber(CompletableFuture<Result<E, T>> future) {
+    this.future = future;
+  }
+  
+  CompletableFuture<Result<E, T>> future() {
+    return future;
+  }
 
+  /**
+   * Joins the fiber, returning a Program that will complete when the fiber completes.
+   *
+   * @param <S> the type of the state
+   * @return a Program that will complete with the result of the fiber
+   */
   public <S> Program<S, E, T> join() {
     return Program.from(future);
   }
 
+  /**
+   * Cancels the fiber, returning a Program that will complete when the fiber is cancelled.
+   *
+   * @param <S> the type of the state
+   * @return a Program that will complete when the fiber is cancelled
+   */
   public <S> Program<S, E, Void> cancel() {
     return Program.task(() -> future.cancel(true));
   }
 
+  /** 
+   * Checks if the fiber has completed.
+   *
+   * @return true if the fiber has completed, false otherwise
+   */
   public boolean isCompleted() {
     return future.isDone();
   }
 
+  /** 
+   * Checks if the fiber has been cancelled.
+   *
+   * @return true if the fiber has been cancelled, false otherwise
+   */
   public boolean isCancelled() {
     return future.isCancelled();
   }
 
+  /**
+   * Maps the fiber, returning a new fiber with the mapped result.
+   *
+   * @param <R> the type of the result of the mapped fiber
+   * @param mapper the function to map the result of the fiber
+   * @return a new fiber with the mapped result
+   */
   public <R> Fiber<E, R> map(Function<T, R> mapper) {
     var mapped = future.thenApply(result -> result.map(mapper));
     return new Fiber<>(mapped);
   }
+  
+  /**
+   * Flat maps the fiber, returning a new fiber that will complete when the
+   * mapped fiber completes.
+   *
+   * @param <R> the type of the result of the mapped fiber
+   * @param mapper the function to map the result of the fiber
+   * @return a new fiber that will complete when the mapped fiber completes
+   */
+  public <R> Fiber<E, R> flatMap(Function<T, Fiber<E, R>> mapper) {
+    var flatMapped = future.thenCompose(result -> result.fold(
+        error -> CompletableFuture.completedFuture(Result.<E, R>failure(error)),
+        value -> mapper.apply(value).future));
+    return new Fiber<>(flatMapped);
+  }
 
+  /**
+   * Waits for all the fibers to complete, returning a new fiber that will complete
+   * when all the fibers have completed.
+   *
+   * @param <E> the type of the error
+   * @param fibers the collection of fibers to wait for
+   * @return a new fiber that will complete when all the fibers have completed
+   */
   public static <E> Fiber<E, Void> all(Collection<? extends Fiber<E, ?>> fibers) {
     var all = CompletableFuture.allOf(fibers.stream().map(Fiber::future).toArray(CompletableFuture[]::new))
         .thenApply(_ -> Result.<E, Void>success(null));
@@ -196,6 +258,18 @@ public record Fiber<E, T>(CompletableFuture<Result<E, T>> future) {
 
   // end generated code
 
+  /**
+   * Creates a new fiber that will complete when either of the two fibers complete.
+   * The result will be an Either containing the result of the completed fiber.
+   * The other fiber will be cancelled.
+   *
+   * @param <E> the type of the error
+   * @param <T> the type of the result of the first fiber
+   * @param <U> the type of the result of the second fiber
+   * @param f1 the first fiber
+   * @param f2 the second fiber
+   * @return a new fiber that will complete when either of the two fibers complete
+   */
   public static <E, T, U> Fiber<E, Either<T, U>> either(Fiber<E, T> f1, Fiber<E, U> f2) {
     return new Fiber<>(f1.future.thenApplyAsync(t -> t.map(Either::<T, U>left))
       .applyToEitherAsync(f2.future.thenApplyAsync(u -> u.map(Either::<T, U>right)), result -> {
